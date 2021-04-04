30597dc733bac423c146128b32852d1024bb01fd
anil <aaerciyas@gmail.com>
2021-04-04 09:52:54 +0300

    question1

--- question1.py
+++ question1.py
@@ -5,43 +5,38 @@ import argparse
 import cv2
 import numpy as np
 
-image1 = cv2.imread("soru1-chest.tif",0)
+def FindHistogram(image, text):
+	cv2.imshow("Original", image)
+	hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+	plt.figure()
+	plt.title("Grayscale Histogram")
+	plt.xlabel("Bins")
+	plt.ylabel("# of Pixels")
+	plt.plot(hist)
+	plt.xlim([0, 256])
+	plt.savefig(text + "-histogram.png")
 
-image2 = cv2.imread("soru1-lena.tif")
+def ContrastStreching(image, text):
+	minmax_img = np.zeros((image.shape[0],image.shape[1]),dtype = 'uint8')
+	 
+	# Loop over the image and apply Min-Max formulae
+	for i in range(image.shape[0]):
+	    for j in range(image.shape[1]):
+	        minmax_img[i,j] = 255*(image[i,j]-np.min(image))/(np.max(image)-np.min(image))
 
-image3 = cv2.imread("soru1-pepper.tif")
+	eq = cv2.equalizeHist(minmax_img)
 
-cv2.imshow("Original", image1)
-hist = cv2.calcHist([image1], [0], None, [256], [0, 256])
-plt.figure()
-plt.title("Grayscale Histogram")
-plt.xlabel("Bins")
-plt.ylabel("# of Pixels")
-plt.plot(hist)
-plt.xlim([0, 256])
-plt.savefig("soru1-chest-histogram.png")
-#plt.show()
+	cv2.imwrite(text+".png",eq)
+	FindHistogram(eq, text)
 
-minmax_img = np.zeros((image1.shape[0],image1.shape[1]),dtype = 'uint8')
- 
-# Loop over the image and apply Min-Max formulae
-for i in range(image1.shape[0]):
-    for j in range(image1.shape[1]):
-        minmax_img[i,j] = 255*(image1[i,j]-np.min(image1))/(np.max(image1)-np.min(image1))
 
-#cv2.imshow('Minmax',minmax_img)
-eq = cv2.equalizeHist(image1)
-#cv2.imshow("Histogram Equalization", eq)
+def main():
+	images = ["soru1-chest.tif", "soru1-lena.bmp", "soru1-pepper.bmp"]
+	for i in images:
+		image = cv2.imread(i, 0)
+		imageName = i.split(".")
+		FindHistogram(image, imageName[0])
+		ContrastStreching(image, imageName[0]+"-final-image")
 
-cv2.imwrite("soru1-chest-final-image.tif",eq)
-hist = cv2.calcHist([image1], [0], None, [256], [0, 256])
-
-plt.figure()
-plt.title("Grayscale Histogram")
-plt.xlabel("Bins")
-plt.ylabel("# of Pixels")
-plt.plot(hist)
-plt.xlim([0, 256])
-plt.savefig("soru1-chest-final-image-histogram.png")
-#plt.show()
-
+if __name__ == "__main__":
+    main()	
